425794de650c51795fdb9d1a0f11f28cf2cb7fb0
skl <1026809406@qq.com>
2020-03-31 16:35:56 +0800

    初始化

---
+++ src/main/java/com/funtl/itoken/zipkin/ZipKinApplication.java
@@ -0,0 +1,20 @@
+package com.funtl.itoken.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import zipkin.server.internal.EnableZipkinServer;
+
+/**
+ * @Author GuardianYruler
+ * @create 2020/3/31 16:20
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class ZipKinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipKinApplication.class, args);
+    }
+}
